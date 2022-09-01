18ec311189252dd0561a128c6d5ef9db88046ee4
Kotik Andrey <Kotik8418@gmail.com>
2022-09-01 18:01:25 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    Console.WriteLine($"Число {number} - палидром");
+
+    else Console.WriteLine($"Число {number} - не палидром");
+}
+
+if (number.Length == 5)
+{
+CheckingNumber(number);
+}
+else Console.WriteLine("Не правильно введено число");
